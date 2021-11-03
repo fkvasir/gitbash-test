d8c5a52774ed05408ac3308faf4b76178bc817e9
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-03 15:41:11 +0800

    added changes

--- qeq.py
+++ qeq.py
@@ -1,3 +1,16 @@
 # git bash test
-# fyi
-# I lOVE CODEX
+"""
+When making new repository
+--> git init
+--> git add .
+--> git commit -m "(anything)"
+--> git remote add origin (https://github.com)
+--> git push --set-upstream origin master
+"""
+
+"""
+When updating a commit
+--> git add .
+--> git commit -m "(anything)"
+--> git push
+"""
